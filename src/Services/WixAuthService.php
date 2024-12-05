b901dfd88849283699e20c3104f9378336b7efe5
Saroj Kala Magar <sarojkalamagar@gmail.com>
2024-12-05 21:02:54 +0545

    Typo issue fixed.

--- src/Services/WixAuthService.php
+++ src/Services/WixAuthService.php
@@ -3,12 +3,13 @@
 namespace StoresSuite\Wix\Services;
 
 use StoresSuite\Wix\Models\WixAccessToken;
+use StoresSuite\Wix\Models\WixRefreshToken;
 
 class WixAuthService
 {
     public function saveRefreshToken(array $tokenDetails)
     {
-        return WixAccessToken::query()->create($tokenDetails);
+        return WixRefreshToken::query()->create($tokenDetails);
     }
 
     public function saveAccessToken(array $tokenDetails)
