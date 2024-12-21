8a2c12cd1c76383b2456b8ffdabe279f30665ff2
Saroj Kala Magar <sarojkalamagar@gmail.com>
2024-12-21 22:21:26 +0545

    Fix: typo

--- src/WixServiceProvider.php
+++ src/WixServiceProvider.php
@@ -16,7 +16,7 @@ class WixServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR_ . '/routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->mergeConfigFrom(__DIR__ . '/config/wix.php', 'wix');
         $this->publishes([ __DIR__ . '/config/wix.php' => $this->app->configPath('wix.php') ], 'config');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
