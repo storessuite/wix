94045a3e595608bb240c83e4a371c8fa03065218
Saroj Kala Magar <sarojkalamagar@gmail.com>
2024-12-22 22:43:45 +0545

    Dependency used in Wix class.

--- src/Wix.php
+++ src/Wix.php
@@ -13,43 +13,54 @@ use StoresSuite\Wix\Services\WixVariantService;
 
 class Wix
 {
+    public function __construct(
+        public WixInstanceService $wixInstanceService,
+        public WixSiteService $wixSiteService,
+        public WixProductService $wixProductService,
+        public WixVariantService $wixVariantService,
+        public WixAuthService $wixAuthService,
+        public WixAppService $wixAppService,
+        public WixCollectionService $wixCollectionService,
+        public WixInventoryService $wixInventoryService,
+    ) {}
+
     public static function instance(): WixInstanceService
     {
-        return new WixInstanceService();
+        return self::$wixInstanceService;
     }
 
     public static function site(): WixSiteService
     {
-        return new WixSiteService();
+        return self::$wixSiteService;
     }
 
     public static function product(): WixProductService
     {
-        return new WixProductService();
+        return self::$wixProductService;
     }
 
     public static function variant(): WixVariantService
     {
-        return new WixVariantService();
+        return self::$wixVariantService;
     }
 
     public static function auth(): WixAuthService
     {
-        return new WixAuthService();
+        return self::$wixAuthService;
     }
 
     public static function app(): WixAppService
     {
-        return new WixAppService();
+        return self::$wixAppService;
     }
 
     public static function collection(): WixCollectionService
     {
-        return new WixCollectionService();
+        return self::$wixCollectionService;
     }
 
     public static function inventory(): WixInventoryService
     {
-        return new WixInventoryService();
+        return self::$wixInventoryService;
     }
 }
