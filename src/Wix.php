d3bf45bd42fe24ffb2fff3601f07a7ef3574b873
Saroj Kala Magar <sarojkalamagar@gmail.com>
2024-12-22 22:51:45 +0545

    Converted static method to non-static.

--- src/Wix.php
+++ src/Wix.php
@@ -13,42 +13,42 @@ use StoresSuite\Wix\Services\WixVariantService;
 
 class Wix
 {
-    public static function instance(WixInstanceService $wixInstanceService): WixInstanceService
+    public function instance(WixInstanceService $wixInstanceService): WixInstanceService
     {
         return $wixInstanceService;
     }
 
-    public static function site(WixSiteService $wixSiteService): WixSiteService
+    public function site(WixSiteService $wixSiteService): WixSiteService
     {
         return $wixSiteService;
     }
 
-    public static function product(WixProductService $wixProductService): WixProductService
+    public function product(WixProductService $wixProductService): WixProductService
     {
         return $wixProductService;
     }
 
-    public static function variant(WixVariantService $wixVariantService): WixVariantService
+    public function variant(WixVariantService $wixVariantService): WixVariantService
     {
         return $wixVariantService;
     }
 
-    public static function auth(WixAuthService $wixAuthService): WixAuthService
+    public function auth(WixAuthService $wixAuthService): WixAuthService
     {
         return $wixAuthService;
     }
 
-    public static function app(WixAppService $wixAppService): WixAppService
+    public function app(WixAppService $wixAppService): WixAppService
     {
         return $wixAppService;
     }
 
-    public static function collection(WixCollectionService $wixCollectionService): WixCollectionService
+    public function collection(WixCollectionService $wixCollectionService): WixCollectionService
     {
         return $wixCollectionService;
     }
 
-    public static function inventory(WixInventoryService $wixInventoryService): WixInventoryService
+    public function inventory(WixInventoryService $wixInventoryService): WixInventoryService
     {
         return $wixInventoryService;
     }
