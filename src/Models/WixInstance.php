28e8b74eba3dbfd404ded8941245a5e17925a7ee
Saroj Kala Magar <sarojkalamagar@gmail.com>
2024-12-24 20:33:59 +0545

    Added fillable properties in WixInstance model.

--- src/Models/WixInstance.php
+++ src/Models/WixInstance.php
@@ -6,7 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class WixInstance extends Model
 {
-    protected $fillable = [];
+    protected $fillable = [
+        'instance_instanceId',
+        'instance_appName',
+        'instance_appVersion',
+        'instance_isFree',
+        'instance_billing_packageName',
+        'instance_billing_billingCycle',
+        'instance_billing_timeStamp',
+        'instance_billing_expirationDate',
+        'instance_billing_autoRenewing',
+        'instance_billing_invoiceId',
+        'instance_billing_source',
+        'instance_billing_freeTrialInfo_status',
+        'instance_billing_freeTrialInfo_endDate',
+        'instance_originInstanceId',
+        'instance_isOriginSiteTemplate',
+        'instance_copiedFromTemplate',
+        'instance_freeTrialAvailable',
+        'site_siteDisplayName',
+        'site_locale',
+        'site_paymentCurrency',
+        'site_multilingual_isMultiLingual',
+        'site_url',
+        'site_description',
+        'site_ownerEmail',
+        'site_ownerInfo_email',
+        'site_ownerInfo_emailStatus',
+        'site_siteId',
+    ];
 
     public function extractSite(): WixSite {}
 }
